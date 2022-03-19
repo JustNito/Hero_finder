a00047504dba82c5a735ff93525a8e2ac1a13e51
Manzharov Nikita <manzharovn@gmail.com>
2022-03-19 12:15:50 +0300

    add number of heroes found

--- app/src/main/java/ru/manzharovn/herofinder/presentation/heroBuilderScreen/HeroBuilderViewModel.kt
+++ app/src/main/java/ru/manzharovn/herofinder/presentation/heroBuilderScreen/HeroBuilderViewModel.kt
@@ -4,38 +4,52 @@ import android.util.Log
 import androidx.compose.runtime.*
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import okhttp3.Dispatcher
 import ru.manzharovn.domain.models.ErrorEntity
+import ru.manzharovn.domain.models.HeroShortDescription
 import ru.manzharovn.domain.models.Power
 import ru.manzharovn.domain.models.Result
 import ru.manzharovn.domain.usecase.GetPowersUseCase
+import ru.manzharovn.domain.usecase.SearchHeroesByPowerUseCase
 import ru.manzharovn.herofinder.presentation.utils.Status
+import kotlin.coroutines.CoroutineContext
 
-class HeroBuilderViewModel(private val getPowersUseCase: GetPowersUseCase) : ViewModel() {
+class HeroBuilderViewModel(
+   private val getPowersUseCase: GetPowersUseCase,
+   private val searchHeroesByPowerUseCase: SearchHeroesByPowerUseCase
+   ) : ViewModel() {
 
    val listOfPower = mutableStateListOf<Power>()
 
+   private var coroutineContext: CoroutineContext? = null
+
    private val chosenPowers = mutableStateListOf<Power>()
+
+   private val heroIds = mutableStateListOf<Int>()
 
    private var _amountOfChosenPowers by mutableStateOf(chosenPowers.size)
    val amountOfChosenPowers: Int
       get() = _amountOfChosenPowers
 
+   private var _amountOfFoundHeroes by mutableStateOf(heroIds.size)
+   val amountOfFoundHeroes: Int
+      get() = _amountOfFoundHeroes
+
    private var _status by mutableStateOf(Status.LOADING)
    val status: Status
       get() = _status
+
+   private var _heroesStatus by mutableStateOf(Status.OK)
+   val heroesStatus: Status
+      get() = _heroesStatus
 
    private fun updateAmountOfChosenPowers() {
       _amountOfChosenPowers = chosenPowers.size
    }
 
-   fun clearChosenPowers() {
-      chosenPowers.clear()
-      updateAmountOfChosenPowers()
+   private fun updateAmountOfFoundHeroes() {
+      _amountOfFoundHeroes = heroIds.size
    }
 
    private fun changeStatusByError(error: ErrorEntity) {
@@ -48,19 +62,56 @@ class HeroBuilderViewModel(private val getPowersUseCase: GetPowersUseCase) : Vie
       }
    }
 
+   fun clearChosenPowers() {
+      chosenPowers.clear()
+      heroIds.clear()
+      updateAmountOfFoundHeroes()
+      updateAmountOfChosenPowers()
+      _heroesStatus = Status.OK
+   }
+
    fun unchoosePower(power: Power){
       chosenPowers.remove(power)
       updateAmountOfChosenPowers()
+      if(chosenPowers.size == 0) {
+         heroIds.clear()
+         coroutineContext?.cancel()
+         _heroesStatus = Status.OK
+         updateAmountOfFoundHeroes()
+      } else {
+         getHeroes()
+      }
    }
 
    fun choosePower(power: Power) {
       chosenPowers.add(power)
       updateAmountOfChosenPowers()
+      getHeroes()
    }
 
    fun isPowerChosen(power: Power): Boolean = chosenPowers.contains(power)
 
-   fun getData() {
+   private fun getHeroes() {
+      coroutineContext?.cancel()
+      heroIds.clear()
+      coroutineContext = viewModelScope.launch(Dispatchers.IO) {
+         _heroesStatus = Status.LOADING
+         when (val result = searchHeroesByPowerUseCase(chosenPowers)) {
+            is Result.Success -> {
+               yield()
+               heroIds.addAll(result.data)
+               Log.i("HeroBuilder", "len: ${heroIds.size}")
+               _heroesStatus = Status.OK
+            }
+            is Result.Error -> {
+               _heroesStatus = Status.NETWORK
+            }
+         }
+         updateAmountOfFoundHeroes()
+      }
+   }
+
+   fun getPowers() {
       if (listOfPower.isEmpty()) {
          viewModelScope.launch(Dispatchers.IO) {
             _status = Status.LOADING
