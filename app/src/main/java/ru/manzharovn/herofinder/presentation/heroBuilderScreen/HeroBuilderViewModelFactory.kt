660869bc33417cab23566bff4dd5ebc078586327
Manzharov Nikita <manzharovn@gmail.com>
2022-03-19 12:12:24 +0300

    add searchHeroesByPowerUseCase dependency

--- app/src/main/java/ru/manzharovn/herofinder/presentation/heroBuilderScreen/HeroBuilderViewModelFactory.kt
+++ app/src/main/java/ru/manzharovn/herofinder/presentation/heroBuilderScreen/HeroBuilderViewModelFactory.kt
@@ -3,14 +3,17 @@ package ru.manzharovn.herofinder.presentation.heroBuilderScreen
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import ru.manzharovn.domain.usecase.GetPowersUseCase
+import ru.manzharovn.domain.usecase.SearchHeroesByPowerUseCase
 import javax.inject.Inject
 
 class HeroBuilderViewModelFactory @Inject constructor(
-        val getPowersUseCase: GetPowersUseCase
+        val getPowersUseCase: GetPowersUseCase,
+        val searchHeroesByPowerUseCase: SearchHeroesByPowerUseCase
     ) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return HeroBuilderViewModel(
-                getPowersUseCase = getPowersUseCase
+            getPowersUseCase = getPowersUseCase,
+            searchHeroesByPowerUseCase = searchHeroesByPowerUseCase
         ) as T
     }
 
