80592508038a111355db8739e6c32bd31bcbdba9
Manzharov Nikita <manzharovn@gmail.com>
2022-03-19 12:17:16 +0300

    change response

--- data/src/main/java/ru/manzharovn/data/datasource/PowersRemoteDataSource.kt
+++ data/src/main/java/ru/manzharovn/data/datasource/PowersRemoteDataSource.kt
@@ -1,12 +1,11 @@
 package ru.manzharovn.data.datasource
 
-import ru.manzharovn.data.models.PowerEntity
 import ru.manzharovn.data.network.ComicVineApi
-import ru.manzharovn.data.network.ComicVineResponse
+import ru.manzharovn.data.network.response.PowerResponse
 import javax.inject.Inject
 
 class PowersRemoteDataSource @Inject constructor(val comicVineApi: ComicVineApi) {
-    suspend fun getPowers(offset: Int = 0): ComicVineResponse<PowerEntity> =
+    suspend fun getPowers(offset: Int = 0): PowerResponse =
         comicVineApi
             .retrofitService
             .getPowers(offset)
