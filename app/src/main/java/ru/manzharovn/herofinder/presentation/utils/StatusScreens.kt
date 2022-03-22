1827f2dab051ede8c1913dee0a106f3c4e84e5d2
Manzharov Nikita <manzharovn@gmail.com>
2022-03-22 09:21:48 +0300

    add navigation component

---
+++ app/src/main/java/ru/manzharovn/herofinder/presentation/utils/StatusScreens.kt
@@ -0,0 +1,47 @@
+package ru.manzharovn.herofinder.presentation.utils
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import ru.manzharovn.herofinder.R
+
+@Composable
+fun LoadingScreen(){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+fun ErrorMessage(status: Status, tryAgain: () -> Unit){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            textAlign = TextAlign.Center,
+            text = "JOPA"
+        )
+        Button(
+            modifier = Modifier.padding(all = 8.dp),
+            onClick = tryAgain
+        ) {
+            Text(stringResource(R.string.try_again_button))
+        }
+    }
+}
